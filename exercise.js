7847731eee7f3c0f7add5e65f78757dbb7e4210b
Carlos Vivas <carlosmuerto@gmail.com>
2022-08-16 13:20:04 -0400

    Add "...expand your function..." part

--- exercise.js
+++ exercise.js
@@ -1,3 +1,7 @@
-const stringLength = (string) => string.length;
+const stringLength = (string) => {
+  if (string.length < 1) throw Error('string most have at lest 1 character');
+  if (string.length > 10) throw Error('string most be not longer than 10 characters');
+  return string.length;
+};
 
 module.exports = { stringLength };
--- exercise.test.js
+++ exercise.test.js
@@ -1,7 +1,15 @@
 const { stringLength } = require('./exercise');
 
 describe('stringLength()', () => {
-  it('"UnitTests" to be 9 ', () => {
+  test('"UnitTests" to be 9 ', () => {
     expect(stringLength('UnitTests')).toBe(9);
+  });
+  test('"" throw "string most have at lest 1 character"', () => {
+    expect(() => { stringLength(''); })
+      .toThrow('string most have at lest 1 character');
+  });
+  test('"To long string" Throw "string most be not longer than 10 characters"', () => {
+    expect(() => { stringLength('To long string'); })
+      .toThrow('string most be not longer than 10 characters');
   });
 });
